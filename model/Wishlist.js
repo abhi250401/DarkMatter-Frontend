4615a27b7814cb02cad71ecfd35c74ff200577b7
Abhishek rai <abhishekrai@euroinfotek.com>
2021-12-01 12:08:35 +0530

    corrected

--- model/Wishlist.js
+++ model/Wishlist.js
@@ -1,14 +1,10 @@
 const mongoose = require('mongoose');
-const stockSchema = new mongoose.Schema({
-    stock_Id: {
-        type: mongoose.Types.ObjectId('Stock')
-    },
 
-}, { _id: false });
+
+
+
 const wishListSchema = new mongoose.Schema({
-    stock: {
-        type: [stockSchema]
-    },
+
     stock_id: {
         type: String
     },
