b00a7a16a3d8a26a8fcf8294f7ccf174808e776c
Abhishek rai <abhishekrai@euroinfotek.com>
2021-12-17 15:16:20 +0530

    kyc

---
+++ src/components/user/kyc.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import axios from "axios";
+import { FormGroup } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+export default function Kyc(props) {
+
+    const [file, setFile] = React.useState('');
+
+    const submitFile = async () => {
+
+        const formData = new FormData()
+        formData.append('profileImg', file)
+        axios.put(process.env.REACT_APP_API_URL + `/user/image/${props.user._id}`, formData, {
+        }).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+
+    }
+    return (
+        <Container maxWidth="xm">
+            <Typography variant="h4">Update Kyc</Typography>
+            <TextField
+                label="Aadhaar Card"
+                name="aadhar"
+
+                margin="normal"
+                variant="outlined"
+                autoComplete="off"
+                fullWidth
+            />
+            <TextField
+                label="PAN Card"
+                name="pan"
+
+                margin="normal"
+                variant="outlined"
+                autoComplete="off"
+                fullWidth
+            />
+            <Stack component="form" noValidate spacing={1}>
+                <TextField
+                    label="Date of Birth"
+                    name="dob"
+
+                    margin="normal"
+                    type="date"
+                    variant="outlined"
+                    autoComplete="off"
+                    fullWidth
+                    defaultValue=""
+                    sx={{ width: 220 }}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+            </Stack>
+            <FormGroup style={{ display: "flex", justifyContent: "center", alignContent: "center", margin: "auto" }}>
+                <input
+                    style={{ display: "flex", justifyContent: "center", alignContent: "center", marginTop: "10px" }}
+                    type="file"
+                    onChange={(e) => setFile(e.target.files[0])}
+                />
+                <button style={{/* width: "100px", marginBottom: "5px", display: "flex", padding: "6px", justifyContent: "center" */ }} onClick={() => submitFile()}> Upload File</button>
+            </FormGroup>
+            <button>Update</button>
+        </Container>
+    );
+};
