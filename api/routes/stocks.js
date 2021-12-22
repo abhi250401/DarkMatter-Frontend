c3daed7c876a3a89bdeb217cbc9b6802c86c5fa3
Tushar Aggarwal <mail@euroinfotek.com>
2021-12-22 22:03:54 +0530

    - Fixed API for stocks

--- api/routes/stocks.js
+++ api/routes/stocks.js
@@ -27,14 +27,53 @@ router.post('/stock', async (req, res) => {
 });
 
 
-router.get('/admin/stocks', async (req, res) => {
+router.get('/admin/stocks', async (req, res, next) => {
+    let page = parseInt(req.query.page) || 0,
+    limit = parseInt(req.query.rowsperpage) || 25,
+    pageOffset = 0,
+    query = {};
 
-    await Stocks.find().then((data) => {
-        res.json(data);
+    if( page > 1 ) {
+        pageOffset = (page-1)*limit;
+    }else {
+        page = 1;
+    }
+
+    let response = { status : 'error', data : [], count : 0, page : page, pages : 1, total : 0, skip : pageOffset };
+
+    //  Get total documents
+    // response.total = await Stocks.find( query ).countDocuments();
+    await Stocks.find( query ).countDocuments()
+    .then( ( total ) => {
+        response.total = total;
+        response.pages = Math.ceil( total / limit );
+        if ( total ) {
+            response.status = 'success';
+        }
+    });
+
+    //  Get paginated documents
+    await Stocks.find( query )
+    .skip( pageOffset )
+    .limit( limit )
+    .then( (data) => {
+        response.data = data;
     }).catch(err => {
-        console.log(err);
-    })
-}
-)
+        response.error = err;
+    }).finally( () => {
+        response.count = response.data.length;
+    });
+
+    /*const agg = Stocks.aggregate([
+        { $match: { public: true }, },
+        { $group: { _id: null, total: { $sum: 1 } } },
+    ]);
+
+    for await (const doc of agg) {
+        console.log(doc);
+    }*/
+
+    return res.status( 200 ).json( response );
+})
 
 module.exports = router;
